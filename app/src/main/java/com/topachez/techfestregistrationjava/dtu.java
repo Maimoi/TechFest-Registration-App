82f88b2ce858b4cdb0517d9ca645eb413c2f2099
mgkbadola <badola3@gmail.com>
2019-11-02 03:44:10 +0530

    Added popup functionality and made changes in resource files.

--- app/src/main/java/com/topachez/techfestregistrationjava/dtu.java
+++ app/src/main/java/com/topachez/techfestregistrationjava/dtu.java
@@ -6,14 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class dtu extends AppCompatActivity {
-      Button b1,b2;
-
+    Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dtu);
+
         b1=findViewById(R.id.btn1a);
         b2=findViewById(R.id.btn1b);
 
