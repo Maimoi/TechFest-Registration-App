51d2ce614c8ec28c534b69598582002ec81ffc5a
mgkbadola <badola3@gmail.com>
2019-11-02 02:03:46 +0530

    See desc. 1.Orientation set to potrait only. 2.Updated college activity files and added nsut file

---
+++ app/src/main/java/com/topachez/techfestregistrationjava/nsut.java
@@ -0,0 +1,14 @@
+package com.topachez.techfestregistrationjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nsut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nsut);
+    }
+}
